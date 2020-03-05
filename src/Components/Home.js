1dc88bc662eadcf172683ef43d5698ffd5defb63
minhajspace <minhaj.space@gmail.com>
2020-03-05 22:24:44 +0530

    layout

---
+++ src/Components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Home.css'
+
+class Home extends React.Component {
+    render() {
+        return <>
+            <div className="flex-container">
+                <div className="aside">
+
+
+                </div>
+
+                <div className="main">
+                    <div>
+                        <h1 className="main-h1">I am</h1><br>
+                        </br><h1 className="main-h1-front"> a Front-end Developer</h1>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </>
+    }
+}
+export default Home;
