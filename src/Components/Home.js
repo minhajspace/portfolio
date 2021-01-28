b54d74645d2d71798dc0e87d618669a6b738f035
minhajspace <minhaj.space@gmail.com>
2021-01-28 21:36:08 +0530

    redesign whole website

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,85 +1,85 @@
-import React from 'react'
-import './Home.css'
-import profile from './image/profile.png'
-import Education from './Education'
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// import React from 'react'
+// import './Home.css'
+// import profile from './image/profile.png'
+// import Education from './Education'
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import facebook from './image/facebook.png'
-import instgram from './image/instagram.png'
-import codesandbox from './image/codesandbox.png'
-import twitter from './image/twitter.png'
-import linkedin from './image/linkedin.png'
-
-
-class Home extends React.Component {
-    render() {
-        return <>
-            <div className="flex-container">
-                <div className="aside">
-                    <img className="profile-picture" alt="profile pic" src={profile} >
-                    </img>
-                    <h2 className="profile-name">
-                        Minhaj Ahmad khan
-                      </h2>
-
-                    <p className="profile-picture-h1">  Front-end Developer in India
-                    </p>
-
-                    <ul>
-                        <Router>
+// import facebook from './image/facebook.png'
+// import instgram from './image/instagram.png'
+// import codesandbox from './image/codesandbox.png'
+// import twitter from './image/twitter.png'
+// import linkedin from './image/linkedin.png'
 
 
+// class Home extends React.Component {
+//     render() {
+//         return <>
+//             <div className="flex-container">
+//                 <div className="aside">
+//                     <img className="profile-picture" alt="profile pic" src={profile} >
+//                     </img>
+//                     <h2 className="profile-name">
+//                         Minhaj Ahmad khan
+//                       </h2>
 
-                            <li className="home-background " > <a href="#">Home</a></li>
+//                     <p className="profile-picture-h1">  Front-end Developer in India
+//                     </p>
 
-                            <li className="services-hover"> <a href="#">skills</a></li>
-
-                            <li className="education-hover"><Link className="link" to="#">Education</Link> </li>
-
-                            <li className="blog-hover"> <a href="#">blog</a></li>
-                            <li className="contact-hover"> <a href="#">Contact Me</a></li>
-                        </Router>
-
-                    </ul>
-                    <div className="social-link-div">
-
-
-                        <ul className="social-media-ul">
-
-
-                            <li > <a href="https://twitter.com/MinhajAhmadKha7" target="blank"><img className=" social-media-connection-picture" alt="twitter logo" src={twitter}></img></a></li>
-                            <li ><a href="https://www.linkedin.com/in/minhajahmadkhan/" target="blank"><img className=" social-media-connection-picture " alt="linkedin logo" src={linkedin}></img></a></li>
-                            <li ><a href="https://www.facebook.com/minhaj.a.khan.14" target="blank"><img className=" social-media-connection-picture" alt="instram logo" src={facebook}></img></a></li>
-                            <li ><a href="https://www.instagram.com/minhaj.mak/" target="blank"><img className=" social-media-connection-picture" alt="instram logo" src={instgram}></img></a></li>
-
-                        </ul>
-                    </div>
+//                     <ul>
+//                         <Router>
 
 
 
-                </div>
+//                             <li className="home-background " > <a href="#">Home</a></li>
+
+//                             <li className="services-hover"> <a href="#">skills</a></li>
+
+//                             <li className="education-hover"><Link className="link" to="#">Education</Link> </li>
+
+//                             <li className="blog-hover"> <a href="#">blog</a></li>
+//                             <li className="contact-hover"> <a href="#">Contact Me</a></li>
+//                         </Router>
+
+//                     </ul>
+//                     <div className="social-link-div">
 
 
-                <div className="main">
-                    <div>
-                        <h1 className="main-h1">I am</h1><br>
-                        </br><h1 className="main-h1-front"> a Front-end Developer</h1>
-                        <p className="long-paragraph">
-                            When I’m working on a problem, I never think about beauty. I think only how to solve the problem. But when I have finished, if the solution is not beautiful, I know it is wrong.<br>
-                            </br>
-                            <em><b className="w3-container w3-center w3-animate-top"> — Freeman Dyson</b> </em>
-                        </p>
-
-                    </div>
-
-                </div>
+//                         <ul className="social-media-ul">
 
 
-            </div>
+//                             <li > <a href="https://twitter.com/MinhajAhmadKha7" target="blank"><img className=" social-media-connection-picture" alt="twitter logo" src={twitter}></img></a></li>
+//                             <li ><a href="https://www.linkedin.com/in/minhajahmadkhan/" target="blank"><img className=" social-media-connection-picture " alt="linkedin logo" src={linkedin}></img></a></li>
+//                             <li ><a href="https://www.facebook.com/minhaj.a.khan.14" target="blank"><img className=" social-media-connection-picture" alt="instram logo" src={facebook}></img></a></li>
+//                             <li ><a href="https://www.instagram.com/minhaj.mak/" target="blank"><img className=" social-media-connection-picture" alt="instram logo" src={instgram}></img></a></li>
+
+//                         </ul>
+//                     </div>
 
 
 
-        </>
-    }
-}
-export default Home;
+//                 </div>
+
+
+//                 <div className="main">
+//                     <div>
+//                         <h1 className="main-h1">I am</h1><br>
+//                         </br><h1 className="main-h1-front"> a Front-end Developer</h1>
+//                         <p className="long-paragraph">
+//                             When I’m working on a problem, I never think about beauty. I think only how to solve the problem. But when I have finished, if the solution is not beautiful, I know it is wrong.<br>
+//                             </br>
+//                             <em><b className="w3-container w3-center w3-animate-top"> — Freeman Dyson</b> </em>
+//                         </p>
+
+//                     </div>
+
+//                 </div>
+
+
+//             </div>
+
+
+
+//         </>
+//     }
+// }
+// export default Home;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './Components/Home'
+import './styles/main.css'
+import App from './Components/App'
 
 
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
