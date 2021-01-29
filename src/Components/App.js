69e219d4cfd8ba7592d0f503b2f89bd1deccce67
minhajspace <minhaj.space@gmail.com>
2021-01-29 19:07:15 +0530

    nav bar complete

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Landing from './Landing'
+import Nav from './Nav'
 
 const App = () => {
     return (
         <div>
+            {/* <Nav /> */}
             <Landing />
         </div>
     )
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,8 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Nav = () => {
+    const [dark, setDark] = useState(false)
+
+    const activeDarkMode = () => {
+        setDark(!dark)
+        console.log(dark)
+    }
     return (
-        <div>
+        <div className="border-2 flex-wrap justify-center flex">
+
+            <div className="border-2 mx-7 my-7 lg:my-0">
+                <div id="profile" className="shadow-2xl opacity-75 mx-6 lg:mx-0 bg-white">
+                    <div className="p-2 md:p-6 text-center lg:text-left">
+                        Home
+                    </div>
+
+                </div>
+            </div>
+            <div className="border-2 mx-7 my-7 lg:my-0">
+                <div id="profile" className="shadow-2xl opacity-75 mx-6 lg:mx-0 bg-white">
+                    <div className="p-2 md:p-6 text-center lg:text-left">
+                        About
+                    </div>
+
+                </div>
+            </div>
+            <div className="border-2 mx-7 my-7 lg:my-0">
+                <div id="profile" className="shadow-2xl opacity-75 mx-6 lg:mx-0 bg-white">
+                    <div className="p-2 md:p-6 text-center lg:text-left">
+                        My Work
+                    </div>
+
+                </div>
+            </div>
+            <div className="border-2 mx-7 my-7 lg:my-0">
+                <div id="profile" className="shadow-2xl opacity-75 mx-6 lg:mx-0 bg-white">
+                    <div className="p-2 md:p-6 text-center lg:text-left">
+                        My Blogs
+                    </div>
+
+                </div>
+            </div>
+            <div className="border-2 mx-7 my-7 lg:my-0">
+                <div id="profile" className="shadow-2xl opacity-75 mx-6 lg:mx-0 bg-white">
+                    <div className="p-2 md:p-6 text-center lg:text-left">
+                        Contact
+                    </div>
+
+                </div>
+            </div>
+            <div className="border-2 mx-7 my-7 lg:my-0">
+                <div id="profile" className="shadow-2xl opacity-75 mx-6 lg:mx-0 bg-white">
+                    <div className="p-2 md:p-6 text-center lg:text-left">
+                        Skills
+                    </div>
+
+                </div>
+            </div>
+
 
         </div>
     )
