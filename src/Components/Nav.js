1aaf9db0122f58e0638073fb90bd65f4f2bd7d73
minhajspace <minhaj.space@gmail.com>
2021-02-01 14:11:10 +0530

    change in nav

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -10,9 +10,9 @@ const Nav = () => {
     }
     return (
         <nav className="flex-wrap justify-center flex">
-            <div className="mx-7 my-7 lg:my-0">
+            <div className="mx-7 my-7 lg:my-0 ">
                 <div id="profile" className=" opacity-75 mx-6 lg:mx-0 bg-white">
-                    <div className="p-2 md:p-6 text-center lg:text-left  hover:border-indigo-600  ">
+                    <div className="p-2 md:p-6 text-center lg:text-left ">
                         <Link to="/">Home </Link>
                     </div>
 
@@ -59,7 +59,7 @@ const Nav = () => {
                 </div>
             </div>
 
-
+            <div className="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"></div>
         </nav>
     )
 }
